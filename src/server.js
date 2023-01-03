0f2656378c8ea185ef32b288dfc5186e773ef114
Nguyen Van Quyet <quyet2k2ocsa@gmail.com>
2023-01-03 17:26:21 +0700

    config static file

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config()
 
-console.log(">>> check env: " , process.env);
+// console.log(">>> check env: " , process.env);
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -11,12 +11,15 @@ const hostname = process.env.HOST_NAME;
 //config template engine
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs')
+// config static file
+app.use(express.static(path.join(__dirname,'public'))) 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/name', (req, res) => {
-  // res.send('<h1> My fullname is Nguyen Van Quyet</h1>')
-  res.render('sample.ejs')
+  // res.send('<h1> My fullname is Nguyen Van Quyet & Nodemon</h1>')
+  res.render('sample.ejs');
 })
 
 app.listen(port, hostname,() => {
